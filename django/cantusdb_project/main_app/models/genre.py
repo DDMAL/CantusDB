e95e8d3b552042b36a5087b9b7ee48d80592df5f
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-05-20 16:07:33 +0000

    add genre model

---
+++ django/cantusdb_project/main_app/models/genre.py
@@ -0,0 +1,19 @@
+from django.contrib.postgres.fields import ArrayField
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.db.models import CheckConstraint, Q
+
+from main_app.models import CustomBaseModel
+
+
+class Genre(CustomBaseModel):
+    mass_office_choices = [
+        ("Mass", "Mass"),
+        ("Office", "Office"),
+        ("Hispanic", "Old Hispanic"),
+    ]
+    name = models.CharField()
+    description = models.TextField()
+    mass_office = ArrayField(
+        base_field=models.CharField(choices=mass_office_choices, max_length=8), size=3
+    )
