5d9615234aaa20c818efac6603f2e97e61148dd5
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-05-20 20:07:39 +0000

    add indexer model

---
+++ django/cantusdb_project/main_app/models/indexer.py
@@ -0,0 +1,10 @@
+from django.db import models
+from main_app.models import CustomBaseModel
+
+
+class Indexer(CustomBaseModel):
+    first_name = models.CharField(max_length=50)
+    family_name = models.CharField(max_length=50)
+    institution = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
