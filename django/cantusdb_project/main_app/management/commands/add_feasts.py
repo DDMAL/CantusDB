9dbd0ab95018ed42a3b9df0f9f370aab83c6290e
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-05-26 13:35:34 -0400

    add management command to add feasts from xml

---
+++ django/cantusdb_project/main_app/management/commands/add_feasts.py
@@ -0,0 +1,54 @@
+from django.core.management.base import BaseCommand, CommandError
+from xml.etree import ElementTree as ET
+import re
+from datetime import datetime
+from main_app.models import Feast
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("xml_file", type=str)
+
+    def handle(self, *args, **options):
+        file_path = options["xml_file"]
+        root = ET.parse(file_path).getroot()
+
+        for feast in root.findall("feast"):
+            name, description, day, month, feast_code, feast_id = (
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+            )
+            for element in feast:
+                element_name = element.tag
+                element_value = feast.find(element.tag).text
+                cleanr = re.compile("<.*?>")
+                element_value = re.sub(cleanr, "", element_value)
+
+                if element_name == "name":
+                    name = element_value
+                if element_name == "description":
+                    description = element_value
+                if element_name == "date":
+                    split_str = element_value.split(".")
+                    if len(split_str) > 1:
+                        day = int(split_str[1])
+                        month = int(datetime.strptime(split_str[0], "%b").month)
+                if element_name == "feast_code":
+                    if element_value != "xxx":
+                        feast_code = int(element_value)
+                if element_name == "id":
+                    feast_id = int(element_value)
+            feast_obj = Feast(
+                id=feast_id,
+                name=name,
+                description=description,
+                feast_code=feast_code,
+                month=month,
+                day=day,
+            )
+            feast_obj.save()
+            print(f"{feast_obj.name} saved on the database!")
