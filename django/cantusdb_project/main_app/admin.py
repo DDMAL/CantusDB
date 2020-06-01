6b262bb4d0a02050c3d717df5211b724e3679b6e
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-06-01 19:02:44 +0000

    register models in admin page

--- django/cantusdb_project/main_app/admin.py
+++ django/cantusdb_project/main_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from main_app.models import *
 # Register your models here.
+admin.site.register(Chant)
+admin.site.register(Feast)
+admin.site.register(Genre)
+admin.site.register(Indexer)
+admin.site.register(Notation)
+admin.site.register(Office)
+admin.site.register(Provenance)
+admin.site.register(Segment)
+admin.site.register(Sequence)
+admin.site.register(Source)
