20f004398560af26e996b3fc644bcc9086d6baa5
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-06-01 14:17:17 -0400

    install flatpages django app
    refs #7

--- django/cantusdb_project/cantusdb/settings.py
+++ django/cantusdb_project/cantusdb/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django.contrib.flatpages",
     "main_app",
     "users",
 ]
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.flatpages.middleware.FlatpageFallbackMiddleware",
 ]
 
 ROOT_URLCONF = "cantusdb.urls"
@@ -123,4 +126,3 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "users.User"
-
