bcfce1e441386f664674ae97e52d50e4c75c585d
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-06-04 17:59:15 -0400

    add template path to settings.py

--- django/cantusdb_project/cantusdb/settings.py
+++ django/cantusdb_project/cantusdb/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = "cantusdb.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -124,5 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 AUTH_USER_MODEL = "users.User"
