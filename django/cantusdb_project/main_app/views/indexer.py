768ebb93344b9a28f3589ef92802d0e6f7c75571
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-06-12 16:04:50 -0400

    add pagination to indexer list view
     refs #5

--- django/cantusdb_project/main_app/views/indexer.py
+++ django/cantusdb_project/main_app/views/indexer.py
@@ -8,6 +8,7 @@ class IndexerDetailView(DetailView):
 
     Accessed by /indexers/<pk>
     """
+
     model = Indexer
     context_object_name = "indexer"
     template_name = "indexer_detail.html"
@@ -16,11 +17,15 @@ class IndexerDetailView(DetailView):
 class IndexerListView(SearchableListMixin, ListView):
     """Searchable List view for Indexer model
     
+    Accessed by /indexers/
+
     When passed a ``?q=<query>`` argument in the GET request, it will filter indexers
     based on the fields defined in ``search_fields`` with the ``icontains`` lookup
     """
+
     model = Indexer
     queryset = Indexer.objects.order_by("family_name")
     search_fields = ["first_name", "family_name", "institution", "city", "country"]
     template_name = "indexer_list.html"
+    paginate_by = 100
     context_object_name = "indexers"
