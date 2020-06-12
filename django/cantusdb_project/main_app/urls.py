6748eb5f247ea09015049c7cf8310962174dc3ce
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-06-12 16:04:56 -0400

    Revert "add pagination to indexer list view"
    This reverts commit 768ebb93344b9a28f3589ef92802d0e6f7c75571.

--- django/cantusdb_project/main_app/urls.py
+++ django/cantusdb_project/main_app/urls.py
@@ -3,6 +3,6 @@ from main_app.views import IndexerDetailView
 from main_app.views import IndexerListView
 
 urlpatterns = [
-    path("indexers/", IndexerListView.as_view(), name="indexer-list"),
-    path("indexers/<int:pk>", IndexerDetailView.as_view(), name="indexer-detail"),
+    path("indexers/", IndexerListView.as_view(), name="article-list"),
+    path("indexers/<int:pk>", IndexerDetailView.as_view(), name="article-detail"),
 ]
