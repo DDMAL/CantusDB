201ff50f3a9a4e0105fff361e2de156224337ef3
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-06-15 15:05:18 -0400

    add feast view url paths
    refs #5, #6

--- django/cantusdb_project/main_app/urls.py
+++ django/cantusdb_project/main_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from main_app.views import IndexerDetailView
 from main_app.views import IndexerListView
+from main_app.views import FeastListView
+from main_app.views import FeastDetailView
 
 urlpatterns = [
     path("indexers/", IndexerListView.as_view(), name="indexer-list"),
     path("indexers/<int:pk>", IndexerDetailView.as_view(), name="indexer-detail"),
+    path("feasts/", FeastListView.as_view(), name="feast-list"),
+    path("feasts/<int:pk>", FeastDetailView.as_view(), name="feast-detail"),
 ]
