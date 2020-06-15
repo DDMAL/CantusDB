4e65c18869b0add20156d71ee31d8ecf579d06c9
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-06-15 15:12:04 -0400

    specify template name in FeastDetailView
    refs #5, #6

--- django/cantusdb_project/main_app/views/feast.py
+++ django/cantusdb_project/main_app/views/feast.py
@@ -5,6 +5,7 @@ from main_app.models import Feast
 class FeastDetailView(DetailView):
     model = Feast
     context_object_name = "feast"
+    template_name = "feast_detail.html"
 
 
 class FeastListView(ListView):
