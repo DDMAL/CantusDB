b6236a1f6777758278ab3ae896193f388ef0d131
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-06-15 15:13:18 -0400

    add SearchableListMixin to FeastListView

--- django/cantusdb_project/main_app/views/feast.py
+++ django/cantusdb_project/main_app/views/feast.py
@@ -1,5 +1,6 @@
 from django.views.generic import DetailView, ListView
 from main_app.models import Feast
+from extra_views import SearchableListMixin
 
 
 class FeastDetailView(DetailView):
@@ -9,7 +10,9 @@ class FeastDetailView(DetailView):
 
 
 class FeastListView(ListView):
+class FeastListView(SearchableListMixin, ListView):
     model = Feast
     queryset = Feast.objects.order_by("name")
     paginate_by = 100
     template_name = "list.html"
+    search_fields = ["name", "feast_code"]
