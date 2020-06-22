128a22435ae40bf1aaf3e509d9d90a83ca2c0a11
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-06-22 12:30:50 -0400

    add office detail and office list views
    refs #5, #6

---
+++ django/cantusdb_project/main_app/views/office.py
@@ -0,0 +1,17 @@
+from django.views.generic import DetailView, ListView
+
+from main_app.models import Office
+
+
+class OfficeDetailView(DetailView):
+    model = Office
+    context_object_name = "office"
+    template_name = "office_detail.html"
+
+
+class OfficeListView(ListView):
+    model = Office
+    queryset = Office.objects.all().order_by("name")
+    paginate_by = 100
+    context_object_name = "offices"
+    template_name = "office_list.html"
