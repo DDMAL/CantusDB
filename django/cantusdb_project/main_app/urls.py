91ccef2594e06cf80bf30d4f9225b0d98765753d
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-06-22 12:31:16 -0400

    add paths for office views
    refs #5, #6

--- django/cantusdb_project/main_app/urls.py
+++ django/cantusdb_project/main_app/urls.py
@@ -5,6 +5,8 @@ from main_app.views import FeastListView
 from main_app.views import FeastDetailView
 from main_app.views import GenreDetailView
 from main_app.views import GenreListView
+from main_app.views import OfficeListView
+from main_app.views import OfficeDetailView
 
 
 urlpatterns = [
@@ -14,4 +16,6 @@ urlpatterns = [
     path("feasts/<int:pk>", FeastDetailView.as_view(), name="feast-detail"),
     path("genres/", GenreListView.as_view(), name="genre-list"),
     path("genres/<int:pk>", GenreDetailView.as_view(), name="genre-detail"),
+    path("offices/", OfficeListView.as_view(), name="office-list"),
+    path("offices/<int:pk>", OfficeDetailView.as_view(), name="office-detail"),
 ]
--- django/cantusdb_project/main_app/views/__init__.py
+++ django/cantusdb_project/main_app/views/__init__.py
@@ -4,3 +4,5 @@ from main_app.views.feast import FeastListView
 from main_app.views.feast import FeastDetailView
 from main_app.views.genre import GenreListView
 from main_app.views.genre import GenreDetailView
+from main_app.views.office import OfficeListView
+from main_app.views.office import OfficeDetailView
