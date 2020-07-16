6829737aeb3622b599306978d71b6e9d4089cd88
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-07-16 12:58:48 -0400

    change siglum model to rismsiglum

--- django/cantusdb_project/main_app/models/rism_siglum.py
+++ django/cantusdb_project/main_app/models/rism_siglum.py
@@ -3,6 +3,6 @@ from django.db import models
 from main_app.models import BaseModel
 
 
-class Siglum(BaseModel):
+class RismSiglum(BaseModel):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
--- django/cantusdb_project/main_app/models/source.py
+++ django/cantusdb_project/main_app/models/source.py
@@ -30,13 +30,9 @@ class Source(BaseModel):
         max_length=255,
         help_text="Full Manuscript Identification (City, Archive, Shelf-mark)",
     )
-    rism_siglum = models.CharField(max_length=50, null=True, blank=True)
-    siglum = models.ForeignKey(
-        "Siglum",
-        on_delete=models.PROTECT,
-        help_text="RISM style siglum + shelf mark",
-        null=True,
-        blank=True,
+    siglum = models.CharField(max_length=50, null=True, blank=True)
+    rism_siglum = models.ForeignKey(
+        "RismSiglum", on_delete=models.PROTECT, null=True, blank=True,
     )
     provenance = models.ForeignKey(
         "Provenance",
