4bdacb0039026179fa37c6540135eeea60ccaa04
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-10-20 18:33:13 -0400

    add django autocomplete light to installed apps

--- django/cantusdb_project/cantusdb/settings.py
+++ django/cantusdb_project/cantusdb/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = os.getenv("CANTUSDB_HOSTS")
 # Application definition
 
 INSTALLED_APPS = [
+    "dal",
+    "dal_select2",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
