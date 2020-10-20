b4bf6823c27e6bb719c0e1319717f1e9e2394d53
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-10-20 18:30:02 -0400

    create ChantSearchView

--- django/cantusdb_project/main_app/views/chant.py
+++ django/cantusdb_project/main_app/views/chant.py
@@ -1,6 +1,6 @@
 from django.views.generic import DetailView, ListView
-
-from main_app.models import Chant
+from django.db.models import Q
+from main_app.models import Chant, Genre, Feast
 
 
 class ChantDetailView(DetailView):
@@ -15,3 +15,49 @@ class ChantListView(ListView):
     paginate_by = 100
     context_object_name = "chants"
     template_name = "chant_list.html"
+
+
+class ChantSearchView(ListView):
+    model = Chant
+    queryset = Chant.objects.all().order_by("id")
+    paginate_by = 100
+    context_object_name = "chants"
+    template_name = "chant_search.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["genres"] = Genre.objects.all().order_by("name").values("id", "name")
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        q_obj_filter = Q()
+        if self.request.GET.get("genre"):
+            genre_id = int(self.request.GET.get("genre"))
+            q_obj_filter &= Q(genre__id=genre_id)
+        if self.request.GET.get("cantus_id"):
+            cantus_id = self.request.GET.get("cantus_id")
+            q_obj_filter &= Q(cantus_id=cantus_id)
+        if self.request.GET.get("mode"):
+            mode = self.request.GET.get("mode")
+            q_obj_filter &= Q(mode=mode)
+        if self.request.GET.get("melodies") in ["true", "false"]:
+            full_source_str = self.request.GET.get("melodies")
+            if full_source_str == "true":
+                q_obj_filter &= Q(melody_id__is_null=False)
+        if self.request.GET.get("feast"):
+            feast = self.request.GET.get("feast")
+            feast = Feast.objects.filter(name=feast)
+            q_obj_filter &= Q(feast=feast)
+        if self.request.GET.get("incipit"):
+            # Make list of terms split on spaces
+            incipit_terms = self.request.GET.get("incipit").split(" ")
+            incipit_q = Q()
+            # For each term, add it to the Q object of each field with an OR operation.
+            # We split the terms so that the words can be separated in the actual
+            # field, allowing for a more flexible search, and a field needs
+            # to match only one of the terms
+            for term in incipit_terms:
+                incipit_q |= Q(incipt__icontains=term)
+            q_obj_filter &= incipit_q
+        return queryset.filter(q_obj_filter)
