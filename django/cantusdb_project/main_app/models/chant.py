f6aaee9c7d53ecc4b80e1cbbdc7d4d48c560ab21
jWang <m13121269577@163.com>
2020-11-29 14:20:43 +0000

    update missing helptext and potential new fields

--- django/cantusdb_project/main_app/models/chant.py
+++ django/cantusdb_project/main_app/models/chant.py
@@ -28,7 +28,7 @@ class Chant(BaseModel):
     finalis = models.CharField(blank=True, null=True, max_length=64)
     extra = models.CharField(blank=True, null=True, max_length=64)
     chant_range = models.CharField(
-        blank=True, null=True, help_text='Example: "1-c-k-4".', max_length=256
+        blank=True, null=True, help_text='Example: "1-c-k-4". Optional field', max_length=256
     )
     # TODO: look into the permissions of this field
     addendum = models.CharField(blank=True, null=True, max_length=256)
@@ -44,7 +44,16 @@ class Chant(BaseModel):
     )
     manuscript_full_text_std_proofread = models.NullBooleanField(blank=True, null=True)
     # TODO: maybe change it to its own model?
-    manuscript_full_text = models.TextField(null=True, blank=True)
+    manuscript_full_text = models.TextField(
+        help_text="Enter the wording, word order and spellings as found in the manuscript"
+        ", with abbreviations resolved to standard words. Use upper-case letters as found"
+        " in the source. Retain “Xpistum” (Christum), “Ihc” (Jesus) and other instances of "
+        "Greek characters with their closest approximations of Latin letters. Some punctuation"
+        " signs and vertical dividing lines | are employed in this field. Repetenda and psalm "
+        "cues can also be recorded here. For more information, contact Cantus Database staff.",
+        null=True, 
+        blank=True
+    )
     manuscript_full_text_proofread = models.NullBooleanField(blank=True, null=True)
     manuscript_syllabized_full_text = models.TextField(null=True, blank=True)
     # TODO: maybe change this to its own model?
@@ -60,3 +69,15 @@ class Chant(BaseModel):
     sylabilized_full_text = models.TextField(blank=True, null=True)
     indexing_notes = models.TextField(blank=True, null=True)
     json_info = JSONField(null=True, blank=True)
+
+    # newly-added fields 2020-11-27
+    # not sure what field type we should use exactly
+    
+    # content_structure = models.CharField(
+    #     blank=True, null=True, max_length=64,
+    #     help_text="Additional folio number field, if folio numbers appear on the leaves but are not in the 'binding order'."
+    #     )
+    # fragmentarium_id = models.CharField(blank=True, null=True, max_length=64)
+    # # Digital Analysis of Chant Transmission
+    # dact = models.CharField(blank=True, null=True, max_length=64)
+    # also a second differentia field
