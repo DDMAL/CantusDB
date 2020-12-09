2a5328ab1b9ae0a19eaf1561426b6b83c185adae
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2020-12-09 16:39:40 -0500

    Update sequence.py

--- django/cantusdb_project/main_app/models/sequence.py
+++ django/cantusdb_project/main_app/models/sequence.py
@@ -3,19 +3,19 @@ from main_app.models import BaseModel
 
 
 class Sequence(BaseModel):
-    title = models.CharField(max_length=100)
-    siglum = models.CharField(blank=True, null=True, max_length=100)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    siglum = models.CharField(blank=True, null=True, max_length=255)
     incpit = models.CharField(blank=True, null=True, max_length=255)
-    folio = models.CharField(blank=True, null=True, max_length=20)
+    folio = models.CharField(blank=True, null=True, max_length=255)
     sequence = models.CharField(blank=True, null=True, max_length=255)
     genre = models.ForeignKey("Genre", blank=True, null=True, on_delete=models.PROTECT)
     rubric = models.CharField(blank=True, null=True, max_length=255)
-    analecta_hymnica = models.CharField(blank=True, null=True, max_length=100)
+    analecta_hymnica = models.CharField(blank=True, null=True, max_length=255)
     indexing_notes = models.TextField(blank=True, null=True)
-    date = models.CharField(blank=True, null=True, max_length=50)
-    ah_volume = models.CharField(blank=True, null=True, max_length=20)
+    date = models.CharField(blank=True, null=True, max_length=255)
+    ah_volume = models.CharField(blank=True, null=True, max_length=255)
     source = models.ForeignKey(
         "Source", blank=True, null=True, on_delete=models.PROTECT
     )
-    cantus_id = models.CharField(max_length=25)
+    cantus_id = models.CharField(blank=True, null=True, max_length=255)
     image_link = models.URLField(blank=True, null=True)
