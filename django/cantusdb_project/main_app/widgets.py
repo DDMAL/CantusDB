1fb678381f1a04faaa120a1eae7a358f666a0e69
jWang <m13121269577@163.com>
2021-01-28 08:39:25 +0000

    cosmetic fix

--- django/cantusdb_project/main_app/widgets.py
+++ django/cantusdb_project/main_app/widgets.py
@@ -1,36 +1,41 @@
 from django.forms.widgets import TextInput, Select, Textarea
 
-class TextInputWidget(TextInput):
 
+class TextInputWidget(TextInput):
     def __init__(self):
         self.attrs = {"class": "form-control form-control-sm"}
 
+
 class SelectWidget(Select):
-    '''
+    """
     not used, this widget does work, but we cannot order the choices by name
-    '''
+    """
+
     def __init__(self):
         attrs = {"class": "form-control custom-select custom-select-sm"}
         super().__init__(attrs=attrs)
-        #super().choices = choices
-        #self.choices = super().choices
+        # super().choices = choices
+        # self.choices = super().choices
 
 
 class TextAreaWidget(Textarea):
     def __init__(self):
         self.attrs = {"class": "form-control", "rows": "3"}
 
+
 class VolpianoAreaWidget(Textarea):
     def __init__(self):
         self.attrs = {
-            "class": "form-control", 
-            "rows": "1.5", 
-            "style": "font-family: Volpiano; font-size: xx-large"
-            }
+            "class": "form-control",
+            "rows": "1.5",
+            "style": "font-family: Volpiano; font-size: xx-large",
+        }
+
 
 class VolpianoInputWidget(TextInput):
     def __init__(self):
         self.attrs = {
-            "class": "form-control form-control-sm", 
-            "style": "font-family: Volpiano; font-size: xx-large"
-            }
+            "class": "form-control form-control-sm",
+            "style": "font-family: Volpiano; font-size: xx-large",
+        }
+
