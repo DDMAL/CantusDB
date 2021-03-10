bbefa9789e2c71988526c352746b6ee1be83777b
Gustavo <gustavo.polinspedro@mail.mcgill.ca>
2021-03-10 08:22:21 +0000

    formatting

--- django/cantusdb_project/main_app/models/sequence.py
+++ django/cantusdb_project/main_app/models/sequence.py
@@ -5,10 +5,12 @@ from main_app.models import BaseModel
 class Sequence(BaseModel):
     title = models.CharField(max_length=255, blank=True, null=True)
     siglum = models.CharField(blank=True, null=True, max_length=255)
-    incpit = models.CharField(blank=True, null=True, max_length=255)
+    incipit = models.CharField(blank=True, null=True, max_length=255)
     folio = models.CharField(blank=True, null=True, max_length=255)
     sequence = models.CharField(blank=True, null=True, max_length=255)
-    genre = models.ForeignKey("Genre", blank=True, null=True, on_delete=models.PROTECT)
+    genre = models.ForeignKey(
+        "Genre", blank=True, null=True, on_delete=models.PROTECT
+    )
     rubric = models.CharField(blank=True, null=True, max_length=255)
     analecta_hymnica = models.CharField(blank=True, null=True, max_length=255)
     indexing_notes = models.TextField(blank=True, null=True)
