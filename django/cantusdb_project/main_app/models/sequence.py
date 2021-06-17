5587a0d1083c76981757879034afac27acbfbc77
jWang <m13121269577@163.com>
2021-06-17 14:03:35 +0000

    update sequence display

--- django/cantusdb_project/main_app/models/sequence.py
+++ django/cantusdb_project/main_app/models/sequence.py
@@ -1,23 +1,29 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 from main_app.models import BaseModel
 
 
 class Sequence(BaseModel):
+    visible_status = models.CharField(max_length=1, blank=True, null=True)
     title = models.CharField(max_length=255, blank=True, null=True)
-    siglum = models.CharField(blank=True, null=True, max_length=255)
     incipit = models.CharField(blank=True, null=True, max_length=255)
+    siglum = models.CharField(blank=True, null=True, max_length=255)
     folio = models.CharField(blank=True, null=True, max_length=255)
     sequence = models.CharField(blank=True, null=True, max_length=255)
-    genre = models.ForeignKey(
-        "Genre", blank=True, null=True, on_delete=models.PROTECT
-    )
-    rubric = models.CharField(blank=True, null=True, max_length=255)
+    genre = models.ForeignKey("Genre", blank=True, null=True, on_delete=models.PROTECT)
+    rubrics = models.CharField(blank=True, null=True, max_length=255)
     analecta_hymnica = models.CharField(blank=True, null=True, max_length=255)
     indexing_notes = models.TextField(blank=True, null=True)
     date = models.CharField(blank=True, null=True, max_length=255)
+
+    col1 = models.CharField(blank=True, null=True, max_length=255)
+    col2 = models.CharField(blank=True, null=True, max_length=255)
+    col3 = models.CharField(blank=True, null=True, max_length=255)
+
     ah_volume = models.CharField(blank=True, null=True, max_length=255)
     source = models.ForeignKey(
         "Source", blank=True, null=True, on_delete=models.PROTECT
     )
     cantus_id = models.CharField(blank=True, null=True, max_length=255)
     image_link = models.URLField(blank=True, null=True)
+    json_info = JSONField(null=True, blank=True)
--- django/cantusdb_project/main_app/views/sequence.py
+++ django/cantusdb_project/main_app/views/sequence.py
@@ -1,5 +1,6 @@
 from django.views.generic import DetailView, ListView
 from main_app.models import Sequence
+from django.db.models import Q
 
 
 class SequenceDetailView(DetailView):
@@ -18,7 +19,12 @@ class SequenceListView(ListView):
     """
 
     model = Sequence
-    queryset = Sequence.objects.all().order_by("id")
     paginate_by = 100
     context_object_name = "sequences"
     template_name = "sequence_list.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        q_obj_filter = Q(source__visible=True)
+        q_obj_filter &= Q(source__public=True)
+        return queryset.filter(q_obj_filter).order_by("siglum", "folio", "sequence")
