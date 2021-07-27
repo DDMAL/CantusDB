56ddcb14a72d6ab1bf06291252beb3c6a786396c
jWang <m13121269577@163.com>
2021-07-27 22:10:20 +0000

    display indexers with at least one public source

--- django/cantusdb_project/main_app/views/indexer.py
+++ django/cantusdb_project/main_app/views/indexer.py
@@ -1,6 +1,9 @@
+from django.db.models.aggregates import Count
+from django.db.models.query_utils import Q
 from django.views.generic import DetailView, ListView
 from main_app.models import Indexer
 from extra_views import SearchableListMixin
+from django.db.models.functions import Lower
 
 
 class IndexerDetailView(DetailView):
@@ -8,6 +11,7 @@ class IndexerDetailView(DetailView):
 
     Accessed by /indexers/<pk>
     """
+
     model = Indexer
     context_object_name = "indexer"
     template_name = "indexer_detail.html"
@@ -21,9 +25,23 @@ class IndexerListView(SearchableListMixin, ListView):
     When passed a ``?q=<query>`` argument in the GET request, it will filter indexers
     based on the fields defined in ``search_fields`` with the ``icontains`` lookup
     """
+
     model = Indexer
-    queryset = Indexer.objects.order_by("family_name")
     search_fields = ["first_name", "family_name", "institution", "city", "country"]
     paginate_by = 100
     template_name = "indexer_list.html"
     context_object_name = "indexers"
+
+    def get_queryset(self):
+        queryset = (
+            super()
+            .get_queryset()
+            .annotate(
+                source_count=Count(
+                    "sources_inventoried", filter=Q(sources_inventoried__public=True)
+                )
+            )
+            .exclude(source_count=0)
+            .order_by(Lower("family_name"))
+        )
+        return queryset
