01ea74f1e09a8429a5bfaaa5397813a00c41f385
jWang <m13121269577@163.com>
2022-02-17 22:55:12 +0000

    update tests for views

--- django/cantusdb_project/main_app/tests/test_views_general.py
+++ django/cantusdb_project/main_app/tests/test_views_general.py
@@ -1,10 +1,29 @@
 from django.urls import reverse
 from django.test import TestCase
-from main_app.models import Indexer, Source, indexer
+
+from main_app.views.feast import FeastListView
 from .make_fakes import *
 
-# python manage.py test main_app.tests.test_views.IndexerListViewTest
-# display deprecation warnings during testing: python -Wa manage.py test
+# run with `python -Wa manage.py test main_app.tests.test_views_genral`
+# the -Wa flag tells Python to display deprecation warnings
+
+
+def get_random_search_term(target):
+    """Helper function for generating a random slice of a string.
+
+    Args:
+        target (str): The content of the field to search.
+    
+    Returns:
+        str: A random slice of `target`
+    """
+    if len(target) <= 2:
+        search_term = target
+    else:
+        slice_start = random.randint(0, len(target) - 2)
+        slice_end = random.randint(slice_start + 2, len(target))
+        search_term = target[slice_start:slice_end]
+    return search_term
 
 
 class IndexerListViewTest(TestCase):
@@ -26,15 +45,11 @@ class IndexerListViewTest(TestCase):
         private_source = Source.objects.create(title="private source", public=False)
         private_source.inventoried_by.set([indexer_with_private_source])
 
-        public_source = Source.objects.create(
-            title="published source",
-            public=True,
-        )
+        public_source = Source.objects.create(title="published source", public=True)
         public_source.inventoried_by.set([indexer_with_public_source])
 
         source_with_multiple_indexers = Source.objects.create(
-            title="private source with multiple indexers",
-            public=False,
+            title="private source with multiple indexers", public=False,
         )
         source_with_multiple_indexers.inventoried_by.set(
             [indexer_with_public_source, indexer_with_private_source]
@@ -47,105 +62,70 @@ class IndexerListViewTest(TestCase):
         self.assertNotIn(indexer_with_private_source, response.context["indexers"])
         self.assertNotIn(indexer_with_no_source, response.context["indexers"])
 
-    def test_search_indexer(self):
+    def test_search_first_name(self):
         """
         Indexer can be searched by passing a `q` parameter to the url \\
         Search fields include first name, family name, country, city, and institution \\
         Only public indexers should appear in the results
         """
-        # generate some indexers
         indexer_with_public_source = make_fake_indexer()
-        indexer_with_private_source = make_fake_indexer()
-        indexer_with_no_source = make_fake_indexer()
-
-        # generate public/private sources and assign indexers to them
-        private_source = Source.objects.create(title="private source", public=False)
-        private_source.inventoried_by.set([indexer_with_private_source])
-
-        public_source = Source.objects.create(
-            title="published source",
-            public=True,
-        )
+        public_source = Source.objects.create(title="published source", public=True,)
         public_source.inventoried_by.set([indexer_with_public_source])
 
-        source_with_multiple_indexers = Source.objects.create(
-            title="private source with multiple indexers",
-            public=False,
-        )
-        source_with_multiple_indexers.inventoried_by.set(
-            [indexer_with_public_source, indexer_with_private_source]
-        )
-
-        # search for the public indexer
-        response = self.client.get(
-            reverse("indexer-list"), {"q": indexer_with_public_source.first_name}
-        )
+        # search with a random slice of first name
+        target = indexer_with_public_source.first_name
+        search_term = get_random_search_term(target)
+        response = self.client.get(reverse("indexer-list"), {"q": search_term})
         self.assertEqual(response.status_code, 200)
         self.assertIn(indexer_with_public_source, response.context["indexers"])
-        self.assertNotIn(indexer_with_private_source, response.context["indexers"])
-        self.assertNotIn(indexer_with_no_source, response.context["indexers"])
 
-        response = self.client.get(
-            reverse("indexer-list"), {"q": indexer_with_public_source.family_name}
-        )
+    def test_search_family_name(self):
+        indexer_with_public_source = make_fake_indexer()
+        public_source = Source.objects.create(title="published source", public=True,)
+        public_source.inventoried_by.set([indexer_with_public_source])
+
+        target = indexer_with_public_source.family_name
+        search_term = get_random_search_term(target)
+        response = self.client.get(reverse("indexer-list"), {"q": search_term})
         self.assertEqual(response.status_code, 200)
         self.assertIn(indexer_with_public_source, response.context["indexers"])
-        self.assertNotIn(indexer_with_private_source, response.context["indexers"])
-        self.assertNotIn(indexer_with_no_source, response.context["indexers"])
 
-        response = self.client.get(
-            reverse("indexer-list"), {"q": indexer_with_public_source.institution}
-        )
+    def test_search_country(self):
+        indexer_with_public_source = make_fake_indexer()
+        public_source = Source.objects.create(title="published source", public=True,)
+        public_source.inventoried_by.set([indexer_with_public_source])
+
+        target = indexer_with_public_source.country
+        search_term = get_random_search_term(target)
+        response = self.client.get(reverse("indexer-list"), {"q": search_term})
         self.assertEqual(response.status_code, 200)
         self.assertIn(indexer_with_public_source, response.context["indexers"])
-        self.assertNotIn(indexer_with_private_source, response.context["indexers"])
-        self.assertNotIn(indexer_with_no_source, response.context["indexers"])
 
-        response = self.client.get(
-            reverse("indexer-list"), {"q": indexer_with_public_source.country}
-        )
+    def test_search_city(self):
+        indexer_with_public_source = make_fake_indexer()
+        public_source = Source.objects.create(title="published source", public=True,)
+        public_source.inventoried_by.set([indexer_with_public_source])
+
+        target = indexer_with_public_source.city
+        search_term = get_random_search_term(target)
+        response = self.client.get(reverse("indexer-list"), {"q": search_term})
         self.assertEqual(response.status_code, 200)
         self.assertIn(indexer_with_public_source, response.context["indexers"])
-        self.assertNotIn(indexer_with_private_source, response.context["indexers"])
-        self.assertNotIn(indexer_with_no_source, response.context["indexers"])
 
-        response = self.client.get(
-            reverse("indexer-list"), {"q": indexer_with_public_source.city}
-        )
+    def test_search_institution(self):
+        indexer_with_public_source = make_fake_indexer()
+        public_source = Source.objects.create(title="published source", public=True,)
+        public_source.inventoried_by.set([indexer_with_public_source])
+
+        target = indexer_with_public_source.institution
+        search_term = get_random_search_term(target)
+        response = self.client.get(reverse("indexer-list"), {"q": search_term})
         self.assertEqual(response.status_code, 200)
         self.assertIn(indexer_with_public_source, response.context["indexers"])
-        self.assertNotIn(indexer_with_private_source, response.context["indexers"])
-        self.assertNotIn(indexer_with_no_source, response.context["indexers"])
-
-        # search for the private indexer
-        response = self.client.get(
-            reverse("indexer-list"), {"q": indexer_with_private_source.first_name}
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertNotIn(indexer_with_private_source, response.context["indexers"])
-
-        # search for the no-source indexer
-        response = self.client.get(
-            reverse("indexer-list"), {"q": indexer_with_no_source.first_name}
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertNotIn(indexer_with_no_source, response.context["indexers"])
-
-        # search for a slice of the first name
-        response = self.client.get(
-            reverse("indexer-list"), {"q": indexer_with_public_source.first_name[:2]}
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(indexer_with_public_source, response.context["indexers"])
-        # use `repr` because `asserQuerysetEqual` will transform every entry in Queryset
-        # and then compare it to the corresponding item in Values, `repr` is the default transform
-        self.assertQuerysetEqual(
-            response.context["indexers"], [repr(indexer_with_public_source)]
-        )
 
 
 class IndexerDetailViewTest(TestCase):
-    def test_url_templates(self):
+    def test_url_and_templates(self):
         """Test the url and templates used"""
         indexer = make_fake_indexer()
         response = self.client.get(reverse("indexer-detail", args=[indexer.id]))
@@ -161,15 +141,113 @@ class IndexerDetailViewTest(TestCase):
 
 
 class FeastListViewTest(TestCase):
-    def test_url_templates(self):
+    def test_url_and_templates(self):
         """Test the url and templates used"""
         response = self.client.get(reverse("feast-list"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "base.html")
         self.assertTemplateUsed(response, "feast_list.html")
 
-    def test_search_feast(self):
-        """Feast can be searched by any part of its name, description, or feastcode"""
+    def test_filter_by_month(self):
+        for i in range(1, 13):
+            Feast.objects.create(name=f"test_feast{i}", month=i)
+        for i in range(1, 13):
+            month = str(i)
+            response = self.client.get(reverse("feast-list"), {"month": month})
+            self.assertEqual(response.status_code, 200)
+            feasts = response.context["feasts"]
+            self.assertTrue(all(feast.month == i for feast in feasts))
+
+    def test_ordering(self):
+        """Feast can be ordered by name or feast_code"""
+        # Order by feast_code
+        response = self.client.get(reverse("feast-list"), {"sort_by": "feast_code"})
+        self.assertEqual(response.status_code, 200)
+        feasts = response.context["feasts"]
+        self.assertEqual(feasts.query.order_by[0], "feast_code")
+
+        # Order by name
+        response = self.client.get(reverse("feast-list"), {"sort_by": "name"})
+        self.assertEqual(response.status_code, 200)
+        feasts = response.context["feasts"]
+        self.assertEqual(feasts.query.order_by[0], "name")
+
+        # Empty ordering parameters in GET request should default to ordering by name
+        response = self.client.get(reverse("feast-list"), {"sort_by": ""})
+        self.assertEqual(response.status_code, 200)
+        feasts = response.context["feasts"]
+        self.assertEqual(feasts.query.order_by[0], "name")
+
+        # Anything other than name and feast_code should default to ordering by name
+        response = self.client.get(
+            reverse("feast-list"), {"sort_by": make_fake_text(max_size=5)}
+        )
+        self.assertEqual(response.status_code, 200)
+        feasts = response.context["feasts"]
+        self.assertEqual(feasts.query.order_by[0], "name")
+
+    def test_search_name(self):
+        """Feast can be searched by any part of its name, description, or feast_code"""
         feast = make_fake_feast()
-        response = self.client.get(reverse("feast-list"))
-        pass
+        target = feast.name
+        search_term = get_random_search_term(target)
+        response = self.client.get(reverse("feast-list"), {"q": search_term})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(feast, response.context["feasts"])
+
+    def test_search_description(self):
+        feast = make_fake_feast()
+        target = feast.description
+        search_term = get_random_search_term(target)
+        response = self.client.get(reverse("feast-list"), {"q": search_term})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(feast, response.context["feasts"])
+
+    def test_search_feast_code(self):
+        feast = make_fake_feast()
+        target = feast.feast_code
+        search_term = get_random_search_term(target)
+        response = self.client.get(reverse("feast-list"), {"q": search_term})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(feast, response.context["feasts"])
+
+    def test_pagination(self):
+        PAGINATE_BY = FeastListView.paginate_by
+        # test 2 full pages of feasts
+        feast_count = PAGINATE_BY * 2
+        for i in range(feast_count):
+            make_fake_feast()
+        page_count = int(feast_count / PAGINATE_BY)
+        assert page_count == 2
+        for page_num in range(1, page_count + 1):
+            response = self.client.get(reverse("feast-list"), {"page": page_num})
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue("is_paginated" in response.context)
+            self.assertTrue(response.context["is_paginated"])
+            self.assertEqual(len(response.context["feasts"]), PAGINATE_BY)
+
+        # test a little more than 2 full pages of feasts
+        new_feast_count = feast_count + random.randint(1, PAGINATE_BY - 1)
+        for i in range(new_feast_count - feast_count):
+            make_fake_feast()
+        new_page_count = page_count + 1
+        # The last page should have the same number of feasts as we added
+        response = self.client.get(reverse("feast-list"), {"page": new_page_count})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("is_paginated" in response.context)
+        self.assertTrue(response.context["is_paginated"])
+        self.assertEqual(len(response.context["feasts"]), new_feast_count - feast_count)
+
+        # test the "last" syntax
+        response = self.client.get(reverse("feast-list"), {"page": "last"})
+        self.assertEqual(response.status_code, 200)
+
+        # Test some invalid values for pages
+        response = self.client.get(reverse("feast-list"), {"page": -1})
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get(reverse("feast-list"), {"page": 0})
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get(reverse("feast-list"), {"page": "lst"})
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get(reverse("feast-list"), {"page": new_page_count + 1})
+        self.assertEqual(response.status_code, 404)
