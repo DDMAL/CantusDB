12bd01ecc79db1e3c0aa17b803012136171985d1
jWang <m13121269577@163.com>
2022-03-30 22:10:27 +0000

    change ALLOWED_HOSTS to list

--- django/cantusdb_project/cantusdb/settings.py
+++ django/cantusdb_project/cantusdb/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("CANTUSDB_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(strtobool(os.getenv("CANTUSDB_DEBUG", "False")))
 
-ALLOWED_HOSTS = os.getenv("CANTUSDB_HOSTS")
+ALLOWED_HOSTS = [os.getenv("CANTUSDB_HOSTS")]
 
 
 # Application definition
