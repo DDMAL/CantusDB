7229bf9b3428e4887f0ef84c199d866a2c3690d5
Jiwoo Jeong <jj.side.projects@gmail.com>
2022-05-16 18:22:08 +0000

    implemented source-create page, formatting not finished yet

--- django/cantusdb_project/main_app/models/rism_siglum.py
+++ django/cantusdb_project/main_app/models/rism_siglum.py
@@ -13,3 +13,7 @@ class RismSiglum(BaseModel):
 
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
