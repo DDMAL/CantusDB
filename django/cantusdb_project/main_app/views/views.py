8ba46eae970eafdce9e0a6791a6e409da396424a
Jacob deGroot-Maggetti <jacob.4ristotle@gmail.com>
2022-05-25 17:48:00 +0000

    update json_node_export to handle requests for data on sequences, indexers

--- django/cantusdb_project/main_app/views/views.py
+++ django/cantusdb_project/main_app/views/views.py
@@ -1,13 +1,14 @@
 import csv
 from multiprocessing.sharedctypes import Value
 from django.http.response import HttpResponseRedirect, JsonResponse
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.urls.base import reverse
 from main_app.models import (
     Chant,
     Sequence,
     Source,
+    Indexer,
 )
 from main_app.forms import ContactForm
 from django.core.mail import send_mail, get_connection
@@ -469,16 +470,29 @@ def json_melody_export(request, cantus_id):
 
 def json_node_export(request, id):
     """
-    returns all fields of the chant/source with the specified `id`
+    returns all fields of the chant/sequence/source/indexer with the specified `id`
     """
-    chant = Chant.objects.filter(id=id)
-    source = Source.objects.filter(id=id)
-    if chant and source:
-        raise ValueError("id is associated with both a chant and a source")
-    elif not chant and not source:
-        raise ValueError("id is associated with neither a chant nor a source")
-    chant_or_source = chant if chant else source
-    vals = dict(*chant_or_source.values())
+    is_chant = is_sequence = is_source = is_indexer = False # these flags will be useful soon for renaming internal variables into their legacy equivalents in old CantusDB
+    
+    requested_item = Chant.objects.filter(id=id) # TODO: refactor: this cascade of conditionals seems very inelegant
+    if requested_item:
+        is_chant = True
+    else:
+        requested_item = Sequence.objects.filter(id=id)
+        if requested_item:
+            is_sequence = True
+        else:
+            requested_item = Source.objects.filter(id=id)
+            if requested_item:
+                is_source = True
+            else:
+                requested_item = Indexer.objects.filter(id=id)
+                if requested_item:
+                    is_indexer = True
+                else:
+                    return HttpResponseNotFound()
+    
+    vals = dict(*requested_item.values())
 
     return JsonResponse(vals)
 
