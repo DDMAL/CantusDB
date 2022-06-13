5ac5f9528c7bef684b35aa8267b150a409558f6f
Jacob deGroot-Maggetti <jacob.4ristotle@gmail.com>
2022-06-13 20:41:30 +0000

    add full_name attribute to User class

--- django/cantusdb_project/users/models.py
+++ django/cantusdb_project/users/models.py
@@ -7,12 +7,17 @@ class User(AbstractUser):
     city = models.CharField(max_length=255, blank=True, null=True)
     country = models.CharField(max_length=255, blank=True, null=True)
     website = models.URLField(blank=True, null=True)
-    name = models.CharField(max_length=255, blank=True, null=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+
+    @property
+    def name(self):
+        if self.full_name:
+            return self.full_name
+        elif self.first_name and self.last_name:
+            return f'{self.first_name} {self.last_name}'
 
     def __str__(self):
         if self.name:
             return self.name
-        elif self.first_name and self.last_name:
-            return f'{self.first_name} {self.last_name}'
         else:
             return self.username
