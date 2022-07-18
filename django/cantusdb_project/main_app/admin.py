b92445031e0b52cfb2580b9fa06c12dd1195c011
Jiwoo Jeong <jj.side.projects@gmail.com>
2022-07-18 20:21:21 +0000

    Customize Source admin page

--- django/cantusdb_project/main_app/admin.py
+++ django/cantusdb_project/main_app/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from main_app.models import *
+from django.contrib.auth import get_user_model
 # Register your models here.
+
+class SourcesUserCanEditInline(admin.TabularInline):
+    model = get_user_model().sources_user_can_edit.through
+
+class SourceAdmin(admin.ModelAdmin):
+    inlines = [SourcesUserCanEditInline]
+    filter_horizontal = ('century', 'notation', 'current_editors', 'inventoried_by', 'full_text_entered_by', 'melodies_entered_by', 'proofreaders', 'other_editors')
+
 admin.site.register(Chant)
 admin.site.register(Feast)
 admin.site.register(Genre)
@@ -10,4 +19,4 @@ admin.site.register(Office)
 admin.site.register(Provenance)
 admin.site.register(Segment)
 admin.site.register(Sequence)
-admin.site.register(Source)
+admin.site.register(Source, SourceAdmin)
