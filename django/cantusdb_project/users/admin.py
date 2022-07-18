c65d3e70282328679dd676c510e0ce89f9f27804
Jiwoo Jeong <jj.side.projects@gmail.com>
2022-07-18 20:37:31 +0000

    Simplify UserAdmin by removing custom forms

--- django/cantusdb_project/users/admin.py
+++ django/cantusdb_project/users/admin.py
@@ -5,31 +5,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 # Register your models here.
 
-class CustomUserCreationForm(UserCreationForm):
-
-    class Meta:
-        model = User
-        fields = (
-            'email', 'password', 'is_active', 'date_joined', 'last_login', 
-            'full_name', 'first_name', 'last_name', 'institution', 'city', 
-            'country', 'website', 'is_staff', 'groups', 'sources_user_can_edit',
-        )
-
-
-class CustomUserChangeForm(UserChangeForm):
-
-    class Meta:
-        model = User
-        fields = (
-            'email', 'password', 'is_active', 'date_joined', 'last_login', 
-            'full_name', 'first_name', 'last_name', 'institution', 'city', 
-            'country', 'website', 'is_staff', 'groups', 'sources_user_can_edit',
-        )
-
-
 class UserAdmin(BaseUserAdmin):
-    form = CustomUserChangeForm
-    add_form = CustomUserCreationForm 
     list_display = ('email', 'first_name', 'last_name', 'institution',)
     list_filter = ('groups',)
     fieldsets = (
