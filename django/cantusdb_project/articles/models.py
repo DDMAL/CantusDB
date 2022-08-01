9bd9e085d83eb519f3247b05749739baee98e71a
4ristotle <jacob.4ristotle@gmail.com>
2022-08-01 15:03:37 +0000

    add author and created_by fields to Article model

--- django/cantusdb_project/articles/models.py
+++ django/cantusdb_project/articles/models.py
@@ -1,5 +1,11 @@
+from django.conf import settings
 from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
+    author = models.CharField(max_length=255)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
