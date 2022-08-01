67269a9397a7409feaffaba5c981299a6447acb8
4ristotle <jacob.4ristotle@gmail.com>
2022-08-01 15:13:22 +0000

    add date_created and date_updated fields to Article model

--- django/cantusdb_project/articles/models.py
+++ django/cantusdb_project/articles/models.py
@@ -4,10 +4,22 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
+    
     author = models.CharField(max_length=255)
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+    )
+
+    date_created = models.DateTimeField(
+        help_text="The date this article was created",
+        null=True,
+        blank=True,)
+    date_updated = models.DateTimeField(
+        help_text="The date this article was most recently updated",
+        auto_now=True,
+        null=True,
+        blank=True,
     )
 
     def __str__(self):
