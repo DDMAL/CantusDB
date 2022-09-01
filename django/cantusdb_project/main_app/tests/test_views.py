7b1f5f17ad3060d8d6eafd37b1edc1aed8fbb87a
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2022-09-01 17:27:43 +0000

    move tests of users from main_app/tests to users/tests.py

--- django/cantusdb_project/main_app/tests/test_views.py
+++ django/cantusdb_project/main_app/tests/test_views.py
@@ -2128,46 +2128,6 @@ class SourceEditViewTest(TestCase):
         self.assertEqual(source.title, 'test')
 
 
-class UserListViewTest(TestCase):
-    def setUp(self):
-        self.user = get_user_model().objects.create(email='test@test.com')
-        self.user.set_password('pass')
-        self.user.save()
-        self.client = Client()
-        self.client.login(email='test@test.com', password='pass')
-
-    def test_url_and_templates(self):
-        response = self.client.get(reverse("user-list"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "user_list.html")
-
-    def test_view(self):
-        for i in range(5):
-            get_user_model().objects.create(email=f"test{i}@test.com")
-
-        response = self.client.get(reverse("user-list"))
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.context["users"]), 6)
-
-
-class UserDetailViewTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        get_user_model().objects.create(email='test@test.com')
-
-    def test_url_and_templates(self):
-        user = get_user_model().objects.first()
-        response = self.client.get(reverse('user-detail', args=[user.id]))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "user_detail.html")
-
-    def test_context(self):
-        user = get_user_model().objects.first()
-        response = self.client.get(reverse('user-detail', args=[user.id]))
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["user"], user)
-
-
 class CISearchViewTest(TestCase):
     def test_view_url_path(self):
         fake_search_term = faker.word()
--- django/cantusdb_project/users/tests.py
+++ django/cantusdb_project/users/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+# run with `python -Wa manage.py test users.tests`
+# the -Wa flag tells Python to display deprecation warnings
+
+
+class UserListViewTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(email='test@test.com')
+        self.user.set_password('pass')
+        self.user.save()
+        self.client = Client()
+        self.client.login(email='test@test.com', password='pass')
+
+    def test_url_and_templates(self):
+        response = self.client.get(reverse("user-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "user_list.html")
+
+    def test_view(self):
+        for i in range(5):
+            get_user_model().objects.create(email=f"test{i}@test.com")
+
+        response = self.client.get(reverse("user-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["users"]), 6)
+
+
+class UserDetailViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        get_user_model().objects.create(email='test@test.com')
+
+    def test_url_and_templates(self):
+        user = get_user_model().objects.first()
+        response = self.client.get(reverse('user-detail', args=[user.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "user_detail.html")
+
+    def test_context(self):
+        user = get_user_model().objects.first()
+        response = self.client.get(reverse('user-detail', args=[user.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["user"], user)
