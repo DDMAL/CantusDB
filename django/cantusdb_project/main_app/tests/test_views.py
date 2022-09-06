3bb084192e3cbcc9b9d82452353d73b1e175572e
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2022-09-06 18:39:58 +0000

    add ChangePasswordViewTest.test_change_password

--- django/cantusdb_project/main_app/tests/test_views.py
+++ django/cantusdb_project/main_app/tests/test_views.py
@@ -2521,11 +2521,19 @@ class ChangePasswordViewTest(TestCase):
         self.client.login(email='test@test.com', password='pass')
 
     def test_url_and_templates(self):
-        response_2 = self.client.get("/change-password/")
-        self.assertEqual(response_2.status_code, 200)
-        self.assertTemplateUsed(response_2, "base.html")
-        self.assertTemplateUsed(response_2, "registration/change_password.html")
         response_1 = self.client.get(reverse("change-password"))
         self.assertEqual(response_1.status_code, 200)
         self.assertTemplateUsed(response_1, "base.html")
         self.assertTemplateUsed(response_1, "registration/change_password.html")
+        response_2 = self.client.get("/change-password/")
+        self.assertEqual(response_2.status_code, 200)
+        self.assertTemplateUsed(response_2, "base.html")
+        self.assertTemplateUsed(response_2, "registration/change_password.html")
+
+    def test_change_password(self):
+        response_1 = self.client.post(reverse("change-password"), {"old_password": "pass", "new_password1": "updated_pass", "new_password2": "updated_pass"})
+        self.assertEqual(response_1.status_code, 200)
+        self.client.logout()
+        self.client.login(email='test@test.com', password='updated_pass')
+        response_2 = self.client.get(reverse("change-password"))
+        self.assertEqual(response_2.status_code, 200) # if login failed, status code will be 302
