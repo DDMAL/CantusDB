f68311c151b52baa1867988d6425cc8ac14942f8
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2022-09-08 15:33:22 +0000

    add update_next_chant_fields to main_app/signals.py

--- django/cantusdb_project/main_app/signals.py
+++ django/cantusdb_project/main_app/signals.py
@@ -45,3 +45,11 @@ def update_source_melody_count(instance, **kwargs):
     source = instance.source
     source.number_of_melodies = source.chant_set.filter(volpiano__isnull=False).count()
     source.save()
+
+@receiver(post_save, sender=Chant)
+@receiver(post_delete, sender=Chant)
+def update_next_chant_fields(instance, **kwargs):
+    """When saving or deleting a Chant, make sure the next_chant of each chant in the source is up-to-date"""
+    source = instance.source
+    for chant in source.chant_set.all():
+        chant.next_chant = chant.get_next_chant()
