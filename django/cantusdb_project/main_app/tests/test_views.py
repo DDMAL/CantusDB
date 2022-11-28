589d23734aae6878d2c4d316b6637c03223e03ac
Jacob deGroot-Maggetti <jacob.degroot-maggetti@mail.mcgill.ca>
2022-11-28 19:14:04 +0000

    update CsvExportTest

--- django/cantusdb_project/main_app/tests/test_views.py
+++ django/cantusdb_project/main_app/tests/test_views.py
@@ -2805,7 +2805,6 @@ class CsvExportTest(TestCase):
             "volpiano",
             "image_link",
             "melody_id",
-            "cao_concordances",
             "addendum",
             "extra",
             "node_id",
@@ -2814,6 +2813,7 @@ class CsvExportTest(TestCase):
             self.assertIn(t, header)
 
         self.assertEqual(len(rows), NUM_CHANTS)
+        self.assertEqual(len(header), len(rows[0]))
     
     def test_published_vs_unpublished(self):
         published_source = make_fake_source(published=True)
