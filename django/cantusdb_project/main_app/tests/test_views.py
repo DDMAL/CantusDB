4abc98bae63fc0ba176e6440e302f8e0ec8b5068
Jacob deGroot-Maggetti <jacob.degroot-maggetti@mail.mcgill.ca>
2022-11-30 15:12:05 +0000

    remove unused import statements from test_views.py

--- django/cantusdb_project/main_app/tests/test_views.py
+++ django/cantusdb_project/main_app/tests/test_views.py
@@ -16,24 +16,19 @@ from .make_fakes import (make_fake_text,
     make_fake_chant,
     make_fake_feast,
     make_fake_genre,
-    make_fake_notation,
     make_fake_office,
     make_fake_provenance,
     make_fake_rism_siglum,
     make_fake_segment,
     make_fake_sequence,
     make_fake_source,
-    make_fake_user,
 )
 
 from main_app.models import Century
 from main_app.models import Chant
 from main_app.models import Feast
 from main_app.models import Genre
-from main_app.models import Notation
 from main_app.models import Office
-from main_app.models import Provenance
-from main_app.models import RismSiglum
 from main_app.models import Segment
 from main_app.models import Sequence
 from main_app.models import Source
