ece094178da50e642c6394dc5439a396e2100343
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2023-03-14 15:56:04 +0000

    add tests to CenturyDetailViewTest

--- django/cantusdb_project/main_app/tests/test_views.py
+++ django/cantusdb_project/main_app/tests/test_views.py
@@ -350,6 +350,35 @@ class CenturyDetailViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "base.html")
         self.assertTemplateUsed(response, "century_detail.html")
+    
+    def test_listed_sources(self):
+        century = make_fake_century()
+        century_sources = [
+            make_fake_source(century=century, published=True)
+            for _ in range(5)
+        ]
+        response = self.client.get(reverse("century-detail", args=[century.id]))
+        returned_sources = response.context["sources"]
+        for source in century_sources:
+            self.assertIn(source, returned_sources)
+
+    def test_unpublished_sources_not_listed(self):
+        century = make_fake_century()
+        published_sources = [
+            make_fake_source(century=century, published=True)
+            for _ in range(5)
+        ]
+        unpublished_sources = [
+            make_fake_source(century=century, published=False)
+            for _ in range(5)
+        ]
+        response = self.client.get(reverse("century-detail", args=[century.id]))
+        returned_sources = response.context["sources"]
+        for source in published_sources:
+            self.assertIn(source, returned_sources)
+        for source in unpublished_sources:
+            self.assertNotIn(source, returned_sources)
+
 
 
 class ChantListViewTest(TestCase):
