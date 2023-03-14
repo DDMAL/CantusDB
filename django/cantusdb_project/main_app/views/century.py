64e5a82c6fd978e806ec9d86984031195c73d413
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2023-03-14 15:20:00 +0000

    Century Detail page: exclude links to source detail pages that user can't visit

--- django/cantusdb_project/main_app/views/century.py
+++ django/cantusdb_project/main_app/views/century.py
@@ -1,7 +1,21 @@
 from django.views.generic import DetailView
-from main_app.models import Century
+from main_app.models import Century, Source
+from typing import Any, Dict
 
 class CenturyDetailView(DetailView):
     model = Century
     context_object_name = "century"
     template_name = "century_detail.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        century = self.get_object()
+        user = self.request.user
+        display_unpublished = user.is_authenticated
+        print(display_unpublished)
+        sources = Source.objects.filter(century=century)
+        if not display_unpublished:
+            print("filtering chants!    ")
+            sources = sources.filter(published=True)
+        context["sources"] = sources
+        return context
