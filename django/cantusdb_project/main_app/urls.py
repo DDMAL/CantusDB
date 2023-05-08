5c4c7d6c876f85e5aae7731aaff77d882325d76c
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2023-05-08 14:53:55 +0000

    improve formatting of main_app/urls.py

--- django/cantusdb_project/main_app/urls.py
+++ django/cantusdb_project/main_app/urls.py
@@ -57,33 +57,75 @@ from main_app.views.user import (
 )
 
 urlpatterns = [
-    path("contact/", views.contact, name="contact"),
+    path(
+        "contact/",
+        views.contact,
+        name="contact",
+    ),
     # login/logout/user
     path(
         "login/",
         CustomLoginView.as_view(redirect_authenticated_user=True),
         name="login",
     ),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
-    path("my-sources/", UserSourceListView.as_view(), name="my-sources"),
-    path("user/<int:pk>", UserDetailView.as_view(), name="user-detail"),
-    path("users/", UserListView.as_view(), name="user-list"),
-    path("change-password/", views.change_password, name="change-password"),
+    path(
+        "logout/",
+        CustomLogoutView.as_view(),
+        name="logout",
+    ),
+    path(
+        "my-sources/",
+        UserSourceListView.as_view(),
+        name="my-sources",
+    ),
+    path(
+        "user/<int:pk>",
+        UserDetailView.as_view(),
+        name="user-detail",
+    ),
+    path(
+        "users/",
+        UserListView.as_view(),
+        name="user-list",
+    ),
+    path(
+        "change-password/",
+        views.change_password,
+        name="change-password",
+    ),
     # century
     path("century/<int:pk>", CenturyDetailView.as_view(), name="century-detail"),
     # chant
     path(
-        "chants/", ChantListView.as_view(), name="chant-list"
+        "chants/",
+        ChantListView.as_view(),
+        name="chant-list",
     ),  # /chants/?source={source id}
-    path("chant/<int:pk>", ChantDetailView.as_view(), name="chant-detail"),
-    path("chant-search/", ChantSearchView.as_view(), name="chant-search"),
     path(
-        "chant-create/<int:source_pk>", ChantCreateView.as_view(), name="chant-create"
+        "chant/<int:pk>",
+        ChantDetailView.as_view(),
+        name="chant-detail",
     ),
     path(
-        "id/<str:cantus_id>", ChantByCantusIDView.as_view(), name="chant-by-cantus-id"
+        "chant-search/",
+        ChantSearchView.as_view(),
+        name="chant-search",
     ),
-    path("chant-delete/<int:pk>", ChantDeleteView.as_view(), name="chant-delete"),
+    path(
+        "chant-create/<int:source_pk>",
+        ChantCreateView.as_view(),
+        name="chant-create",
+    ),
+    path(
+        "id/<str:cantus_id>",
+        ChantByCantusIDView.as_view(),
+        name="chant-by-cantus-id",
+    ),
+    path(
+        "chant-delete/<int:pk>",
+        ChantDeleteView.as_view(),
+        name="chant-delete",
+    ),
     path(
         "edit-chants/<int:source_id>",
         SourceEditChantsView.as_view(),
@@ -100,27 +142,67 @@ urlpatterns = [
         name="source-edit-syllabification",
     ),
     path(
-        "index/", ChantIndexView.as_view(), name="chant-index"
+        "index/",
+        ChantIndexView.as_view(),
+        name="chant-index",
     ),  # /index/?source={source id}
     # feast
-    path("feasts/", FeastListView.as_view(), name="feast-list"),
-    path("feast/<int:pk>", FeastDetailView.as_view(), name="feast-detail"),
+    path(
+        "feasts/",
+        FeastListView.as_view(),
+        name="feast-list",
+    ),
+    path(
+        "feast/<int:pk>",
+        FeastDetailView.as_view(),
+        name="feast-detail",
+    ),
     # genre
-    path("genres/", GenreListView.as_view(), name="genre-list"),
-    path("genre/<int:pk>", GenreDetailView.as_view(), name="genre-detail"),
+    path(
+        "genres/",
+        GenreListView.as_view(),
+        name="genre-list",
+    ),
+    path(
+        "genre/<int:pk>",
+        GenreDetailView.as_view(),
+        name="genre-detail",
+    ),
     # indexer
-    path("indexers/", IndexerListView.as_view(), name="indexer-list"),
+    path(
+        "indexers/",
+        IndexerListView.as_view(),
+        name="indexer-list",
+    ),
     # notation
-    path("notation/<int:pk>", NotationDetailView.as_view(), name="notation-detail"),
+    path(
+        "notation/<int:pk>",
+        NotationDetailView.as_view(),
+        name="notation-detail",
+    ),
     # office
-    path("offices/", OfficeListView.as_view(), name="office-list"),
-    path("office/<int:pk>", OfficeDetailView.as_view(), name="office-detail"),
+    path(
+        "offices/",
+        OfficeListView.as_view(),
+        name="office-list",
+    ),
+    path(
+        "office/<int:pk>",
+        OfficeDetailView.as_view(),
+        name="office-detail",
+    ),
     # provenance
     path(
-        "provenance/<int:pk>", ProvenanceDetailView.as_view(), name="provenance-detail"
+        "provenance/<int:pk>",
+        ProvenanceDetailView.as_view(),
+        name="provenance-detail",
     ),
     # sequence
-    path("sequences/", SequenceListView.as_view(), name="sequence-list"),
+    path(
+        "sequences/",
+        SequenceListView.as_view(),
+        name="sequence-list",
+    ),
     path(
         "sequence/<int:pk>",
         SequenceDetailView.as_view(),
@@ -132,23 +214,57 @@ urlpatterns = [
         name="sequence-edit",
     ),
     # source
-    path("sources/", SourceListView.as_view(), name="source-list"),
-    path("source/<int:pk>", SourceDetailView.as_view(), name="source-detail"),
-    path("source-create/", SourceCreateView.as_view(), name="source-create"),
-    path("edit-source/<int:source_id>", SourceEditView.as_view(), name="source-edit"),
+    path(
+        "sources/",
+        SourceListView.as_view(),
+        name="source-list",
+    ),
+    path(
+        "source/<int:pk>",
+        SourceDetailView.as_view(),
+        name="source-detail",
+    ),
+    path(
+        "source-create/",
+        SourceCreateView.as_view(),
+        name="source-create",
+    ),
+    path(
+        "edit-source/<int:source_id>",
+        SourceEditView.as_view(),
+        name="source-edit",
+    ),
     # melody
-    path("melody/", MelodySearchView.as_view(), name="melody-search"),
-    path("ajax/melody/<str:cantus_id>", views.ajax_melody_list, name="ajax-melody"),
+    path(
+        "melody/",
+        MelodySearchView.as_view(),
+        name="melody-search",
+    ),
+    path(
+        "ajax/melody/<str:cantus_id>",
+        views.ajax_melody_list,
+        name="ajax-melody",
+    ),
     path(
         "ajax/melody-search/",
         views.ajax_melody_search,
         name="ajax-melody-search",
     ),
     # json api
-    path("json-sources/", views.json_sources_export, name="json-sources-export"),
-    path("json-node/<str:id>", views.json_node_export, name="json-node-export"),
     path(
-        "json-nextchants/<str:cantus_id>", views.json_nextchants, name="json-nextchants"
+        "json-sources/",
+        views.json_sources_export,
+        name="json-sources-export",
+    ),
+    path(
+        "json-node/<str:id>",
+        views.json_node_export,
+        name="json-node-export",
+    ),
+    path(
+        "json-nextchants/<str:cantus_id>",
+        views.json_nextchants,
+        name="json-nextchants",
     ),
     path(
         "json-melody/<str:cantus_id>",
@@ -161,15 +277,27 @@ urlpatterns = [
         ChantSearchMSView.as_view(),
         name="chant-search-ms",
     ),
-    path("ci-search/<str:search_term>", CISearchView.as_view(), name="ci-search"),
+    path(
+        "ci-search/<str:search_term>",
+        CISearchView.as_view(),
+        name="ci-search",
+    ),
     path(
         "ajax/search-bar/<str:search_term>",
         views.ajax_search_bar,
         name="ajax-search-bar",
     ),
     # misc
-    path("content-statistics", views.items_count, name="items-count"),
-    path("csv/<str:source_id>", views.csv_export, name="csv-export"),
+    path(
+        "content-statistics",
+        views.items_count,
+        name="items-count",
+    ),
+    path(
+        "csv/<str:source_id>",
+        views.csv_export,
+        name="csv-export",
+    ),
     path(
         "ajax/concordance/<str:cantus_id>",
         views.ajax_concordance_list,
