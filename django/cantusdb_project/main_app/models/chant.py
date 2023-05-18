8daca2c592878627c63d52df932f74bf6dc441ff
Lucas <lucas.march@mail.mcgill.ca>
2023-05-18 19:11:25 +0000

    Fix typo in docstring

--- django/cantusdb_project/main_app/models/chant.py
+++ django/cantusdb_project/main_app/models/chant.py
@@ -153,7 +153,7 @@ class Chant(BaseChant):
     def get_syllabized_text_and_melody(self, text_only=False):
         """Returns the syllabized text with the melody of a chant. Syllabized text splits the words from the
          full text into its syllable components based on the volpiano protocols
-         (ex: Ocundare plebs fidelis cuius pater => I-O-cun-da-re plebs fi-de-lis cu-ius pa-ter)
+         (ex: IOcundare plebs fidelis cuius pater => I-O-cun-da-re plebs fi-de-lis cu-ius pa-ter)
          If volpiano is not established return None
 
                         Preview of melody and text:
