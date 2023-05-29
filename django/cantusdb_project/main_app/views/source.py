969d8994fb36caca39e9533b78b6009dc3f3feae
Lucas <lucas.march@mail.mcgill.ca>
2023-05-29 20:55:30 +0000

    prefetch query objects in source list view

--- django/cantusdb_project/main_app/views/source.py
+++ django/cantusdb_project/main_app/views/source.py
@@ -220,7 +220,14 @@ class SourceListView(ListView):
             )
             q_obj_filter &= indexing_search_q
 
-        return queryset.filter(q_obj_filter).distinct()
+        print(Century.objects.all().order_by("name").values("id", "name"))
+        return (
+            queryset.filter(q_obj_filter)
+            .distinct()
+            .prefetch_related(
+                Prefetch("century", queryset=Century.objects.all().order_by("name"))
+            )
+        )
 
 
 class SourceCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
