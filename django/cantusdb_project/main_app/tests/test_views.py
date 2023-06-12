33c076a83a3128bcd8244d5cf59df97d394c0b8b
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2023-06-12 19:35:25 +0000

    add tests to ensure unpublished sources not displayed on provenance detail pages

--- django/cantusdb_project/main_app/tests/test_views.py
+++ django/cantusdb_project/main_app/tests/test_views.py
@@ -2747,6 +2747,31 @@ class ProvenanceDetailViewTest(TestCase):
         self.assertTemplateUsed(response, "base.html")
         self.assertTemplateUsed(response, "provenance_detail.html")
 
+    def test_listed_sources(self):
+        provenance = make_fake_provenance()
+        provenance_sources = [
+            make_fake_source(provenance=provenance, published=True) for _ in range(5)
+        ]
+        response = self.client.get(reverse("provenance-detail", args=[provenance.id]))
+        returned_sources = response.context["sources"]
+        for source in provenance_sources:
+            self.assertIn(source, returned_sources)
+
+    def test_unpublished_sources_not_listed(self):
+        provenance = make_fake_provenance()
+        published_sources = [
+            make_fake_source(provenance=provenance, published=True) for _ in range(5)
+        ]
+        unpublished_sources = [
+            make_fake_source(provenance=provenance, published=False) for _ in range(5)
+        ]
+        response = self.client.get(reverse("provenance-detail", args=[provenance.id]))
+        returned_sources = response.context["sources"]
+        for source in published_sources:
+            self.assertIn(source, returned_sources)
+        for source in unpublished_sources:
+            self.assertNotIn(source, returned_sources)
+
 
 class SequenceListViewTest(TestCase):
     def setUp(self):
