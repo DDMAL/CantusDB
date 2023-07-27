82d159ac6b916e3742283f72ba197d4519e88bb5
Lucas <lucas.march@mail.mcgill.ca>
2023-07-27 14:10:53 +0000

    Clean up sync source creation date script

--- django/cantusdb_project/main_app/management/commands/sync_source_creation_date.py
+++ django/cantusdb_project/main_app/management/commands/sync_source_creation_date.py
@@ -3,24 +3,15 @@ from main_app.models import (
     Source,
 )
 import requests, json
-import datetime
+from datetime import datetime
 from django.utils import timezone
+from typing import Optional
 
 
-def get_source_list():
-    source_list = []
-    file = open("source_list.txt", "r")
-    for line in file:
-        line = line.strip("\n")
-        source_list.append(line)
-    file.close()
-    return source_list
-
-
-def convert_epoch_to_date_time(epoch):
+def convert_epoch_to_date_time(epoch: str) -> Optional[datetime]:
     try:
         epoch_time = float(epoch)
-        datetime_str = datetime.datetime.fromtimestamp(epoch_time)
+        datetime_str = datetime.fromtimestamp(epoch_time)
         datetime_obj = timezone.make_aware(datetime_str, timezone.utc)
     except (ValueError, TypeError) as e:
         print(f"Error converting epoch time: {e}")
@@ -28,21 +19,19 @@ def convert_epoch_to_date_time(epoch):
     return datetime_obj
 
 
-def update_date_created(source_id):
-    url = f"http://cantus.uwaterloo.ca/json-node/{source_id}"
+def update_date_created(source: Source) -> Source:
+    url = f"http://cantus.uwaterloo.ca/json-node/{source.id}"
     response = requests.get(url)
     json_response = json.loads(response.content)
     try:
         date_created_epoch = json_response["created"]
-    except (KeyError, TypeError) as e:
-        print(f"Exception for source {source_id}: {e}")
-        date_created_epoch = None
-    if date_created_epoch is not None:
-        datetime_str = convert_epoch_to_date_time(date_created_epoch)
-        if datetime_str is not None:
-            Source.objects.filter(id=source_id).update(date_created=datetime_str)
-            source_obj = Source.objects.get(id=source_id)
-        return source_obj
+    except (KeyError, TypeError):
+        return source
+    datetime_str = convert_epoch_to_date_time(date_created_epoch)
+    if datetime_str is not None:
+        source.date_created = datetime_str
+        source.save()
+    return source
 
 
 class Command(BaseCommand):
@@ -50,38 +39,31 @@ class Command(BaseCommand):
         parser.add_argument(
             "id",
             type=str,
-            help="update one source (<source_id>)) or update all sources ('all')",
+            help="update one source (<source_id>) or update all sources ('all')",
         )
 
     def handle(self, *args, **options):
         # before running this, run sync_indexers first
         id = options["id"]
         if id == "all":
-            all_sources = get_source_list()
+            all_sources = Source.objects.all()
             total = len(all_sources)
             count = 0
-            for source_id in all_sources:
-                try:
-                    source = Source.objects.get(id=source_id)
-                except Source.DoesNotExist:
-                    print(f"Source with ID {source_id} does not exist")
-                    source = None
-                if source is not None:
+            for source in all_sources:
+                print(f"old date created for source {source.id}: {source.date_created}")
+                source_updated = update_date_created(source)
+                print(
+                    f"new date created for source {source.id}: {source_updated.date_created}"
+                )
+                if count % 100 == 0:
                     print(
-                        f"old date created for source {source_id}: {source.date_created}"
+                        f"------------------- {count} of {total} sources updated -------------------"
                     )
-                    source_updated = update_date_created(source_id)
-                    print(
-                        f"new date created for source {source_id}: {source_updated.date_created}"
-                    )
-                    if count % 100 == 0:
-                        print(
-                            f"------------------- {count} of {total} sources updated -------------------"
-                        )
-                    count += 1
+                count += 1
+                print()
 
         else:
             source = Source.objects.get(id=id)
             print(f"old date created for source {id}: {source.date_created}")
-            source_updated = update_date_created(id)
+            source_updated = update_date_created(source)
             print(f"new date created for source {id}: {source_updated.date_created}")
