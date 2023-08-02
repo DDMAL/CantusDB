05658ae81275a274a3652e080af3d04f74d2d320
Lucas <lucas.march@mail.mcgill.ca>
2023-08-02 20:31:25 +0000

    update widgets for indexer fields in source admin area

--- django/cantusdb_project/main_app/forms.py
+++ django/cantusdb_project/main_app/forms.py
@@ -863,15 +863,43 @@ class AdminSourceForm(forms.ModelForm):
             | Q(groups__name="editor")
             | Q(groups__name="contributor")
         )
-        .order_by("last_name"),
+        .order_by("full_name"),
         required=False,
-        widget=FilteredSelectMultiple(verbose_name="Century", is_stacked=False),
+        widget=FilteredSelectMultiple(verbose_name="current editors", is_stacked=False),
+    )
+
+    inventoried_by = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all().order_by("full_name"),
+        required=False,
+        widget=FilteredSelectMultiple(verbose_name="inventoried by", is_stacked=False),
+    )
+
+    full_text_entered_by = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all().order_by("full_name"),
+        required=False,
+        widget=FilteredSelectMultiple(
+            verbose_name="full text entered by", is_stacked=False
+        ),
     )
 
     melodies_entered_by = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all().order_by("full_name"),
         required=False,
-        widget=FilteredSelectMultiple(verbose_name="Century", is_stacked=False),
+        widget=FilteredSelectMultiple(
+            verbose_name="melodies entered by", is_stacked=False
+        ),
+    )
+
+    proofreaders = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all().order_by("full_name"),
+        required=False,
+        widget=FilteredSelectMultiple(verbose_name="proofreaders", is_stacked=False),
+    )
+
+    other_editors = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all().order_by("full_name"),
+        required=False,
+        widget=FilteredSelectMultiple(verbose_name="other editors", is_stacked=False),
     )
 
     TRUE_FALSE_CHOICES_INVEN = ((True, "Complete"), (False, "Incomplete"))
