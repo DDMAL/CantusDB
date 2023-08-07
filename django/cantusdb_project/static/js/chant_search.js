062c81783b8db95d45f32919cb19fc9db265e413
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2023-08-07 19:34:11 +0000

    Chant Search: change chant_search.js to automatically populate keyword/cantusID field upon search_bar search

--- django/cantusdb_project/static/js/chant_search.js
+++ django/cantusdb_project/static/js/chant_search.js
@@ -1,10 +1,15 @@
+function containsOnlyLettersAndSpaces(str) {
+    return /^[A-Za-z\s]*$/.test(str);
+  }
+
 window.addEventListener("load", function () {
     // Make sure the select components keep their values across multiple GET requests
     // so the user can "drill down" on what they want
     const opFilter = document.getElementById("opFilter");
     const genreFilter = document.getElementById("genreFilter");
     const melodiesFilter = document.getElementById("melodiesFilter");
-    const searchBar = document.getElementById("searchBar")
+    const keywordField = document.getElementById("keywordSearch");
+    const cantusIDField = document.getElementById("cantus_id");
 
     const urlParams = new URLSearchParams(window.location.search);
     if (urlParams.has("op")) {
@@ -17,6 +22,15 @@ window.addEventListener("load", function () {
         melodiesFilter.value = urlParams.get("melodies");
     }
     if (urlParams.has("search_bar")) {
-        searchBar.value = urlParams.get("search_bar");
+        search_term = urlParams.get("search_bar");
+        if (containsOnlyLettersAndSpaces(search_term)) {
+            // assume user is doing an incipit search
+            opFilter.value = "starts_with"
+            keywordField.value = search_term
+        } else {
+            // if search term contains other characters, assume user
+            // is doing a Cantus ID search
+            cantusIDField.value = search_term
+        }
     }
 });
