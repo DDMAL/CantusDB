3853c57888468983b5d7a084d07271362efd293b
lucasmarchd01 <lucasmarchd01@gmail.com>
2023-08-16 18:01:24 +0000

    permissions: rename variables

--- django/cantusdb_project/main_app/permissions.py
+++ django/cantusdb_project/main_app/permissions.py
@@ -11,11 +11,13 @@ def user_can_edit_chants_in_source(user, source) -> bool:
         return False
 
     source_id = source.id
-    user_is_assigned_to_source = user.sources_user_can_edit.filter(id=source_id)
+    user_is_assigned_to_source: bool = user.sources_user_can_edit.filter(
+        id=source_id
+    ).exists()
 
-    user_is_project_manager = user.groups.filter(name="project manager").exists()
-    user_is_editor = user.groups.filter(name="editor").exists()
-    user_is_contributor = user.groups.filter(name="contributor").exists()
+    user_is_project_manager: bool = user.groups.filter(name="project manager").exists()
+    user_is_editor: bool = user.groups.filter(name="editor").exists()
+    user_is_contributor: bool = user.groups.filter(name="contributor").exists()
 
     return (
         (user_is_project_manager)
@@ -31,9 +33,7 @@ def user_is_proofreader(user) -> bool:
     Checks if the user is a proofreader.
     Used in UserSourceListView.
     """
-    if user.groups.filter(Q(name="project manager") | Q(name="editor")).exists():
-        return True
-    return False
+    return user.groups.filter(Q(name="project manager") | Q(name="editor")).exists()
 
 
 def user_can_proofread_chants_in_source(user, source_id) -> bool:
@@ -43,12 +43,14 @@ def user_can_proofread_chants_in_source(user, source_id) -> bool:
     """
     if user.is_anonymous:
         return False
-    assigned_to_source = user.sources_user_can_edit.filter(id=source_id)
+    user_is_assigned_to_source: bool = user.sources_user_can_edit.filter(
+        id=source_id
+    ).exists()
 
-    is_project_manager = user.groups.filter(name="project manager").exists()
-    is_editor = user.groups.filter(name="editor").exists()
+    user_is_project_manager: bool = user.groups.filter(name="project manager").exists()
+    user_id_editor: bool = user.groups.filter(name="editor").exists()
 
-    if (is_project_manager) or (is_editor and assigned_to_source):
+    if (user_is_project_manager) or (user_id_editor and user_is_assigned_to_source):
         return True
     return False
 
@@ -59,11 +61,11 @@ def user_can_view_unpublished_source(user, source) -> bool:
     to unpublished sources on the site.
     Used in ChantDetail, SequenceDetail, and SourceDetail views.
     """
-    display_unpublished = user.is_authenticated
+    user_is_authenticated: bool = user.is_authenticated
     if (
         (source is not None)
         and (source.published is False)
-        and (not display_unpublished)
+        and (not user_is_authenticated)
     ):
         return False
     return True
@@ -74,11 +76,7 @@ def user_can_edit_sequence(user) -> bool:
     Checks if the user has permission to edit a Sequence object.
     Used in SequenceDetail and SequenceEdit views.
     """
-    is_project_manager = user.groups.filter(name="project manager").exists()
-
-    if is_project_manager:
-        return True
-    return False
+    return user.groups.filter(name="project manager").exists()
 
 
 def user_can_create_source(user) -> bool:
@@ -87,13 +85,9 @@ def user_can_create_source(user) -> bool:
     Used in SourceCreateView.
     """
 
-    is_authorized = user.groups.filter(
+    return user.groups.filter(
         Q(name="project manager") | Q(name="editor") | Q(name="contributor")
     ).exists()
-
-    if is_authorized:
-        return True
-    return False
 
 
 def user_can_edit_source(user, source) -> bool:
@@ -106,9 +100,9 @@ def user_can_edit_source(user, source) -> bool:
     source_id = source.id
     assigned_to_source = user.sources_user_can_edit.filter(id=source_id)
 
-    is_project_manager = user.groups.filter(name="project manager").exists()
-    is_editor = user.groups.filter(name="editor").exists()
-    is_contributor = user.groups.filter(name="contributor").exists()
+    is_project_manager: bool = user.groups.filter(name="project manager").exists()
+    is_editor: bool = user.groups.filter(name="editor").exists()
+    is_contributor: bool = user.groups.filter(name="contributor").exists()
 
     if (
         (is_project_manager)
@@ -133,6 +127,4 @@ def user_can_manage_source_editors(user) -> bool:
     Checks if the user has permission to change the editors assigned to a Source.
     Used in SourceDetailView.
     """
-    if user.is_staff or user.groups.filter(name="project manager").exists():
-        return True
-    return False
+    return user.is_staff or user.groups.filter(name="project manager").exists()
