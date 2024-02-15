db4aafecd186abdb9f715bcff3d5152242b2ab31
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2024-02-15 21:27:32 +0000

    Add tests for incipit signals

--- django/cantusdb_project/main_app/tests/test_models.py
+++ django/cantusdb_project/main_app/tests/test_models.py
@@ -231,6 +231,23 @@ class ChantModelTest(TestCase):
         self.assertEqual(chant1.get_next_chant(), lacuna)
         self.assertEqual(lacuna.get_next_chant(), chant3)
 
+    def test_incipit_signal(self):
+        """Test whether a chant's incipit is updated to reflect its fulltext upon save"""
+        chant: Chant = make_fake_chant()
+        full_text: str = "Incipit should be five words sheep headphones bongoes"
+        expected_incipit: str = "Incipit should be five words"
+        chant.manuscript_full_text_std_spelling = full_text
+        print(
+            f"before refresh\n  {chant.manuscript_full_text_std_spelling=}\n  {chant.incipit=}"
+        )
+        chant.save()
+        chant.refresh_from_db()
+        print(
+            f"refreshed:\n  {chant.manuscript_full_text_std_spelling=}\n  {chant.incipit=}"
+        )
+        observed_incipit: str = chant.incipit
+        self.assertEqual(observed_incipit, expected_incipit)
+
 
 class FeastModelTest(TestCase):
     @classmethod
@@ -348,6 +365,19 @@ class SequenceModelTest(TestCase):
         chant_fields = Chant.get_fields_and_properties()
         seq_fields = Sequence.get_fields_and_properties()
         self.assertEqual(chant_fields, seq_fields)
+
+    def test_incipit_signal(self):
+        """Test whether a sequence's incipit is updated to reflect its fulltext upon save"""
+        sequence: Sequence = make_fake_sequence()
+        full_text: str = (
+            "Incipit debet esse quinque verba bombus equus armadillo macropus"
+        )
+        expected_incipit: str = "Incipit debet esse quinque verba"
+        sequence.manuscript_full_text_std_spelling = full_text
+        sequence.save()
+        sequence.refresh_from_db()
+        observed_incipit: str = sequence.incipit
+        self.assertEqual(observed_incipit, expected_incipit)
 
 
 class SourceModelTest(TestCase):
