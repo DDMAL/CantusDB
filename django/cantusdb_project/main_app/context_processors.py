576af0883b82f9790508f6f9570ba2a5c3e6393f
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2024-03-26 17:38:40 +0000

    context_processors.py: Refactor determine_project_environment
    to fetch the value of PROJECT_ENVIRONMENT directly from the project settings, rather than by reading it from an environment variable.

--- django/cantusdb_project/main_app/context_processors.py
+++ django/cantusdb_project/main_app/context_processors.py
@@ -1,10 +1,18 @@
-import os
+from django.conf import settings
 
 
-def determine_project_environment(request):
-    project_environment: str = os.getenv("PROJECT_ENVIRONMENT")
-    # in cantusdb/settings.py, we've already checked that PROJECT_ENVIRONMENT is
-    # either PRODUCTION, STAGING or DEVELOPMENT
+def determine_project_environment(request) -> str:
+    """_summary_
+
+    Args:
+        request: a request.
+
+    Returns:
+        str: A string indicating the current value of the
+        PROJECT_ENVIRONMENT variable, which is read from an environment
+        variable in settings.py.
+
+    """
     return {
-        "PROJECT_ENVIRONMENT": project_environment,
+        "PROJECT_ENVIRONMENT": settings.PROJECT_ENVIRONMENT,
     }
