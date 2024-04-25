a5c8602b6859c5f33cf36e89574adb685cd504b4
Jacob deGroot-Maggetti <58090591+jacobdgm@users.noreply.github.com>
2024-04-25 20:00:01 +0000

    Chant Create: refactor Suggested Chants feature to use a single JS function

--- django/cantusdb_project/static/js/chant_create.js
+++ django/cantusdb_project/static/js/chant_create.js
@@ -8,3 +8,25 @@ window.addEventListener("load", function () {
         document.getElementById('id_manuscript_full_text').value = standardText;
     }
 })
+
+function autoFillSuggestedChant(genreName, genreID, cantusID, fullText) {
+    document.getElementById('id_cantus_id').value = cantusID;
+    document.getElementById('id_manuscript_full_text_std_spelling').value = fullText;
+
+    // in case suggestion.genre_id was None, don't try to change the #id_genre selector
+    if (genreID === null) {
+        return
+    }
+
+    // Since we're using a django-autocomplete-light widget for the Genre selector,
+    // we need to follow a special process in selecting a value from the widget:
+    // Set the value, creating a new option if necessary
+    if ($('#id_genre').find("option[value='" + genreID + "']").length) {
+        $('#id_genre').val(genreID).trigger('change');
+    } else { 
+        // Create a DOM Option and pre-select by default
+        var newOption = new Option(genreName, genreID, true, true);
+        // Append it to the select
+        $('#id_genre').append(newOption).trigger('change');
+    };
+}
