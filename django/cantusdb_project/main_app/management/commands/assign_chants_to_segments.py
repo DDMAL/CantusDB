816cadb898c535f5ccc582768912bd1e1d8f7046
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-05-08 07:47:46 -0400

    Chunk assign_chants_to_segments command

--- django/cantusdb_project/main_app/management/commands/assign_chants_to_segments.py
+++ django/cantusdb_project/main_app/management/commands/assign_chants_to_segments.py
@@ -9,6 +9,8 @@ all chants in the database to the segment of the source they belong to.
 from django.core.management.base import BaseCommand
 from main_app.models import Source, Chant, Segment
 
+CHANT_CHUNK_SIZE = 1_000
+
 
 class Command(BaseCommand):
     help = "Assigns all chants in the database to the segment of the source they belong to."
@@ -18,6 +20,11 @@ class Command(BaseCommand):
         for source in sources:
             segment = Segment.objects.get(id=source.segment_id)
             chants = Chant.objects.filter(source=source)
-            for chant in chants:
-                chant.segment = segment
-                chant.save()
+            chants_count = chants.count()
+            start_index = 0
+            while start_index < chants_count:
+                chant_chunk = chants[start_index : start_index + CHANT_CHUNK_SIZE]
+                for chant in chant_chunk:
+                    chant.segment = segment
+                    chant.save()
+                start_index += CHANT_CHUNK_SIZE
