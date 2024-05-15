bfa43a40aed54ba5c69054dd0fbcc94b1033ea0c
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-05-15 09:41:23 -0400

    Use update rather than save in assign_chants_to_segments

--- django/cantusdb_project/main_app/management/commands/assign_chants_to_segments.py
+++ django/cantusdb_project/main_app/management/commands/assign_chants_to_segments.py
@@ -9,8 +9,6 @@ all chants in the database to the segment of the source they belong to.
 from django.core.management.base import BaseCommand
 from main_app.models import Source, Chant, Segment
 
-CHANT_CHUNK_SIZE = 1_000
-
 
 class Command(BaseCommand):
     help = "Assigns all chants in the database to the segment of the source they belong to."
@@ -20,11 +18,4 @@ class Command(BaseCommand):
         for source in sources:
             segment = Segment.objects.get(id=source.segment_id)
             chants = Chant.objects.filter(source=source)
-            chants_count = chants.count()
-            start_index = 0
-            while start_index < chants_count:
-                chant_chunk = chants[start_index : start_index + CHANT_CHUNK_SIZE]
-                for chant in chant_chunk:
-                    chant.segment = segment
-                    chant.save()
-                start_index += CHANT_CHUNK_SIZE
+            chants.update(segment=segment)
