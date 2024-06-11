75cf1560443783d29c8014f088ab9c857158459d
Andrew Hankinson <andrew.hankinson@gmail.com>
2024-06-11 15:44:25 +0200

    Fixed: Expand institution model
    After a review of the existing institution information in Cantus, the following improvements are made
    
    - Adds region, country, and alternate names fields for the institution
    - Modifies the admin site for institutions to show the table, and to show an inline for adding identifiers

--- django/cantusdb_project/main_app/admin/institution.py
+++ django/cantusdb_project/main_app/admin/institution.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 
 from main_app.admin.base_admin import BaseModelAdmin
-from main_app.models import Institution
+from main_app.models import Institution, InstitutionIdentifier
+
+
+class InstitutionIdentifierInline(admin.TabularInline):
+    model = InstitutionIdentifier
+    extra = 0
+    exclude = ["created_by", "last_updated_by"]
 
 
 @admin.register(Institution)
 class InstitutionAdmin(BaseModelAdmin):
-    pass
+    list_display = ("name", "siglum", "get_city_region", "country")
+    search_fields = ("name", "siglum", "city", "region", "alternate_names")
+    list_filter = ("city",)
+    inlines = (InstitutionIdentifierInline,)
+
+    def get_city_region(self, obj):
+        name = [obj.city]
+        if obj.region:
+            name.append(f"({obj.region})")
+        return " ".join(name)
+
+    get_city_region.short_description = "City"
--- django/cantusdb_project/main_app/models/institution.py
+++ django/cantusdb_project/main_app/models/institution.py
@@ -3,10 +3,26 @@ from django.db import models
 from main_app.models import BaseModel
 
 
+region_help_text = """Province / State / Canton / County. Used to disambiguate cities, e.g., "London (Ontario)"."""
+city_help_text = """City / Town / Village / Settlement"""
+
+
 class Institution(BaseModel):
     name = models.CharField(max_length=255, default="s.n.")
     siglum = models.CharField(max_length=32, default="XX-Nn")
-    city = models.CharField(max_length=64, blank=True, null=True)
+    city = models.CharField(
+        max_length=64, blank=True, null=True, help_text=city_help_text
+    )
+    region = models.CharField(
+        max_length=64, blank=True, null=True, help_text=region_help_text
+    )
+    country = models.CharField(max_length=64, default="s.l.")
+    alternate_names = models.TextField(
+        blank=True, null=True, help_text="Enter alternate names on separate lines."
+    )
 
-    def __str__(self):
-        return f"{self.name} ({self.siglum})"
+    def __str__(self) -> str:
+        names: list = [self.name]
+        if self.siglum:
+            names.append(f"({self.siglum})")
+        return " ".join(names)
