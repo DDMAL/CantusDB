d4a58e181bbfc02a11248316999690ce9a484b59
Andrew Hankinson <andrew.hankinson@gmail.com>
2024-06-17 21:18:43 +0200

    New: mark institutions as private collectors

--- django/cantusdb_project/main_app/models/institution.py
+++ django/cantusdb_project/main_app/models/institution.py
@@ -5,11 +5,16 @@ from main_app.models import BaseModel
 
 region_help_text = """Province / State / Canton / County. Used to disambiguate cities, e.g., "London (Ontario)"."""
 city_help_text = """City / Town / Village / Settlement"""
+private_collector_help = """Mark this institution as private collector."""
 
 
 class Institution(BaseModel):
     name = models.CharField(max_length=255, default="s.n.")
     siglum = models.CharField(max_length=32, default="XX-Nn")
+    is_private_collector = models.BooleanField(
+        default=False,
+        help_text=private_collector_help,
+    )
     city = models.CharField(
         max_length=64, blank=True, null=True, help_text=city_help_text
     )
@@ -22,6 +27,9 @@ class Institution(BaseModel):
     )
     former_sigla = models.TextField(
         blank=True, null=True, help_text="Enter former sigla on separate lines."
+    )
+    private_notes = models.TextField(
+        blank=True, null=True, help_text="Notes about this institution that are not publicly visible."
     )
 
     def __str__(self) -> str:
