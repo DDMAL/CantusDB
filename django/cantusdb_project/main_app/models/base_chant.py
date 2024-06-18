2dfc484145a43d4bba9e3c6d52f10fe36e2d0999
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-06-18 09:07:05 -0400

    feat!(models): add Project model
    Remove segment field from chants and sequences
    Add project field to chants and sequences

--- django/cantusdb_project/main_app/models/base_chant.py
+++ django/cantusdb_project/main_app/models/base_chant.py
@@ -176,12 +176,15 @@ class BaseChant(BaseModel):
     # this field, populated by the populate_is_last_chant_in_feast script, exists in order to optimize .get_suggested_feasts() on the chant-create page
     is_last_chant_in_feast = models.BooleanField(blank=True, null=True)
 
-    segment = models.ForeignKey(
-        "Segment",
+    project = models.ForeignKey(
+        "Project",
         on_delete=models.PROTECT,
         null=True,
         blank=True,
-        help_text="The segment of the manuscript that contains this chant",
+        help_text=(
+            "The project this chant belongs to. If left blank,"
+            "this chant is considered part of the Cantus (default) project."
+        ),
     )
     # fragmentarium_id = models.CharField(blank=True, null=True, max_length=64)
     # # Digital Analysis of Chant Transmission
