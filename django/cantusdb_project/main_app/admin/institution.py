f6c020890c4b54850405ae686b34e9759c0a5ca7
Andrew Hankinson <andrew.hankinson@gmail.com>
2024-06-20 18:08:50 +0200

    New: Add a list of sources attached to the institution
    Adds a section that shows a list of sources that are attached to the institution at the bottom of the institution page. Also adds the ID as a link field to that source record.

--- django/cantusdb_project/main_app/admin/institution.py
+++ django/cantusdb_project/main_app/admin/institution.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from main_app.admin.base_admin import BaseModelAdmin
-from main_app.models import Institution, InstitutionIdentifier
+from main_app.models import Institution, InstitutionIdentifier, Source
+
+
+class InstitutionSourceInline(admin.TabularInline):
+    model = Source
+    extra = 0
+    fields = ("link_id_field", "shelfmark",)
+    readonly_fields = ("link_id_field",)
+
+    def link_id_field(self, obj):
+        change_url = reverse('admin:main_app_source_change', args=(obj.pk,))
+        return mark_safe(f'<a href="{change_url}">{obj.pk}</a>')
+
 
 
 class InstitutionIdentifierInline(admin.TabularInline):
