c9df0d6044edb313e0009e97113a2faf30aa1431
Lucas <lucas.march@mail.mcgill.ca>
2024-11-04 16:01:00 +0000

    refactor: remove isinstance check

--- django/cantusdb_project/cantusindex.py
+++ django/cantusdb_project/cantusindex.py
@@ -112,11 +112,8 @@ def get_suggested_fulltext(cantus_id: str) -> Optional[str]:
         # mostly, in case of a timeout within get_json_from_ci_api
         return None
 
-    info: Optional[dict] = json_response.get("info", {})
-    if not isinstance(info, dict):
-        return None
-    suggested_fulltext: Optional[str] = info.get("field_full_text")
-    return suggested_fulltext
+    info: Optional[dict] = json_response.get("info", {}) or {}
+    return info.get("field_full_text")
 
 
 def get_merged_cantus_ids() -> Optional[list[Optional[dict]]]:
