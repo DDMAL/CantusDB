1ed26c03efcdc7e3a40418471dbd75a15eeb597e
Dylan Hillerbrand <dhillerbrand@gmail.com>
2025-03-03 11:26:09 -0500

    fix(make_fakes): add segment values to segment_m2m field
    When making a fake source, add the value of the fake source's `segment` field to the `segment_m2m` field.

--- django/cantusdb_project/main_app/tests/make_fakes.py
+++ django/cantusdb_project/main_app/tests/make_fakes.py
@@ -499,6 +499,7 @@ def make_fake_source(**kwargs: Any) -> Source:
     source.melodies_entered_by.set([make_fake_user()])
     source.proofreaders.set([make_fake_user()])
     source.other_editors.set([make_fake_user()])
+    source.segment_m2m.set([kwargs["segment"]])
 
     return source
 
