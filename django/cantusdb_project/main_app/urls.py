67c08d28fec1188d91a85f0100d8fa8b2fca5c15
Lucas <lucas.march@mail.mcgill.ca>
2025-05-27 16:34:46 +0000

    feat: Integrate Universal Viewer for IIIF Manifests Adds a Universal Viewer page to display IIIF images. On the Source detail page: - Links to the new viewer are generated for each IIIF Manifest URL associated with a Source. Related to #1789, #1790

--- django/cantusdb_project/main_app/urls.py
+++ django/cantusdb_project/main_app/urls.py
@@ -7,6 +7,7 @@ from django.contrib.auth.views import (
     PasswordResetConfirmView,
     PasswordResetCompleteView,
 )
+from django.views.generic import TemplateView
 from main_app.views.api import (
     ajax_melody_list,
     ajax_melody_search,
@@ -377,6 +378,12 @@ urlpatterns = [
         "source/<int:source_id>/add-image-links",
         SourceAddImageLinksView.as_view(),
         name="source-add-image-links",
+    ),
+    # universal viewer
+    path(
+        "viewer/",
+        TemplateView.as_view(template_name="uv.html"),
+        name="universal-viewer",
     ),
     # melody
     path(
