670a4eab1c211372032042770b2b2e78e45da678
Kyrie <kyrieb@gmail.com>
2026-04-02 16:17:06 +0000

    fix: correctly import COALESCE to resolve sorting mismatch, adjust import locations to follow PEP8/python convention.

--- django/cantusdb_project/main_app/views/source.py
+++ django/cantusdb_project/main_app/views/source.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.db.models import Q, Prefetch, QuerySet, Value
+from django.db.models.functions import Coalesce
 from django.http import (
     HttpResponseRedirect,
     Http404,
@@ -23,6 +24,7 @@ from django.views.generic import (
     FormView,
 )
 from django.views.generic.detail import SingleObjectMixin
+
 from main_app.forms import (
     SourceCreateForm,
     SourceEditForm,
@@ -43,7 +45,6 @@ from main_app.models import (
 )
 from main_app.permissions import CustomAccessMixin
 from main_app.mixins import JSONResponseMixin
-
 from main_app.views.chant import get_feast_selector_options
 from main_app.tasks import save_browse_chants_formset
 
