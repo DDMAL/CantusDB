05540581374cf23292d8529e9957b2dc9333036b
PouyaMohseni <pouyamohseniii@gmail.com>
2026-07-27 11:16:24 -0400

    feat: add Non-inventoried filter to Browse Sources page
    Resolves #461

--- django/cantusdb_project/main_app/views/source.py
+++ django/cantusdb_project/main_app/views/source.py
@@ -66,6 +66,7 @@ SOURCE_ADVANCED_SEARCH_FIELDS: tuple[str, ...] = (
     "provenance",
     "prodMethod",
     "indexing",
+    "nonInventoried",
 )
 
 
@@ -518,6 +519,8 @@ class SourceListView(CustomAccessMixin, ListView):  # type: ignore[type-arg]
             q_obj_filter &= Q(source_completeness__in=source_completeness)
         if production_method := self.request.GET.get("prodMethod"):
             q_obj_filter &= Q(production_method=production_method)
+        if self.request.GET.get("nonInventoried") == "true":
+            q_obj_filter &= Q(number_of_chants__isnull=True) | Q(number_of_chants=0)
 
         if general_str := self.request.GET.get("general"):
             # Strip leading/trailing spaces and collapse internal whitespace
